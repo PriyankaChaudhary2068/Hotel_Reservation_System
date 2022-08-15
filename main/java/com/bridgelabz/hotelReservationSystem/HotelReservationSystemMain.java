b4ad63a5170d41854ee1f2e7126993840b1e5620
chutki12 <priyankachaudhary2068>
2022-08-15 12:45:59 +0530

    ADDED : Welcome To Hotel Reservation Program

---
+++ main/java/com/bridgelabz/hotelReservationSystem/HotelReservationSystemMain.java
@@ -0,0 +1,11 @@
+package com.bridgelabz.hotelReservationSystem;
+
+public class HotelReservationSystemMain {
+
+	public static void main(String[] args) {
+		
+		System.out.println("Welcome To Hotel Reservation Program !");
+
+	}
+
+}
